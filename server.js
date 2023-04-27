fe9bf64ae6d38fe1cd33cb9b1d4af1ef42adcf6f
sahithisaraswathi <sahithisaraswathisadhu@gmail.com>
2023-04-28 03:56:02 +0530

    first

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 var path=require('path')
 const app = express();
 require("dotenv").config();
-const cors = require('cors')
+// const cors = require('cors')
 const multer = require('multer')
 var morgan=require('morgan')
 var rfs=require('rotating-file-stream')
@@ -10,8 +10,7 @@ var Users = require('./models/userModel');
 var Doctor = require('./models/doctorModel');
 const bodyparser = require("body-parser");
 app.use(bodyparser.json());
-const swaggerJSDoc=require('swagger-jsdoc')
-// const swaggerSpec=require("./swagger.json")
+
 const swaggerUi = require('swagger-ui-express')
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
@@ -25,36 +24,12 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage });
 app.use(express.json());
 
-const options={
-  definition:{
-    openapi: '3.0.0',
-    info : {
-      title: 'Swagger',
-      version: '1.0.0'
-    },
-    servers:[
-      {
-        url: 'http://localhost:5000/'
-      }
-    ]
-  },
-  apis:['./server.js']
-}
-
-
-const swaggerSpec= swaggerJSDoc(options)
-app.use('/api-docs',swaggerUi.serve,swaggerUi.setup(swaggerSpec))
-
 
 
 
 app.get('/',(req,res)=>{
   res.send('Welcome to mongo db')
 })
-
-// app.get('/api/doctor/get-appointments-by-doctor-id',(req,res)=>{
-//   res.render()
-// })
 
 app.post('/image',upload.single('file'),function(req,res){
   if (!req.file) {
