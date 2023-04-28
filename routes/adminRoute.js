00be5a665b38975744a8c1f58d38145b1caa62ed
sairamya31 <101963603+sairamya31@users.noreply.github.com>
2023-04-28 12:41:42 +0530

    Update adminRoute.js redis

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -5,6 +5,26 @@ const Doctor = require('../models/doctorModel');
 const Appointment= require('../models/appointmentModel')
 const authMiddleware = require("../middlewares/authMiddleware");
 
+ const redis=require('redis')
+const client = redis.createClient({
+    password: 'MHNBgRgXXhGxZJ7ymOkBugFhBdUozx38',
+    socket: {
+        host: 'redis-16553.c251.east-us-mz.azure.cloud.redislabs.com',
+        port: 16553
+    }
+});
+
+client.connect();
+client.on('connect', function() {
+    console.log('Redis connected successfully');
+});
+
+client.on('error', function (err) {
+    console.log('Error: ' + err);
+});
+
+
+
 router.get("/get-all-doctors", async (req, res) => {
 
 const doctors=await Doctor.find();
