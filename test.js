3bd506f01070c3c3b57419bbd3626e222f97246b
SahithiSaraswathi <86319356+SahithiSaraswathi@users.noreply.github.com>
2023-04-28 09:41:25 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,18 @@
-// import axios from 'axios'
-// axios.get("http://localhost:5000/api/user/get-all-users").then(response=>{
-//     console.log("hi")
-// })
+const assert = require('assert');
+const login = require('../client/log_test.js');
+
+describe('Login', function() {
+  describe('#authenticate()', function() {
+    it('should return true if the username and password are valid', function() {
+      assert.equal(login.authenticate('user@gmail.com', '12345'), true);
+    });
+
+    it('should return false if the username is invalid', function() {
+      assert.equal(login.authenticate('invalidusername', 'password123'), false);
+    });
+
+    it('should return false if the password is invalid', function() {
+      assert.equal(login.authenticate('lalitha.s20@iiits.in', ''), false);
+    });
+  });
+});
